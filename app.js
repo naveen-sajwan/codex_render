296a800f5366f670466cfa4618e53bfd2605a4a5
naveen sajwan <85891281+naveen-sajwan@users.noreply.github.com>
2026-01-27 21:16:40 +0530

    eighth commit

--- app.js
+++ app.js
@@ -68,12 +68,9 @@ import mail from "./routes/mail.js";
 import favouriteRoute from "./routes/favorite.js";
 import dashboardRoute from "./routes/dashboard.js";
 
-// ---------------- ES Modules __dirname fix ----------------
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // ----------------- App setup -----------------
 const app = express();
+
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
@@ -91,8 +88,9 @@ mongoose.connect(process.env.MONGODB_URI)
 // app.use("/niko/v1", uploadRoute);
 // app.use("/niko/v1", virusRoute);
 
-app.use(cors());
-app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/niko",pdfRoute);
 app.use("/niko/v1",auth);
 app.use("/niko/v1",mail);
@@ -100,21 +98,12 @@ app.use("/niko/v1",favouriteRoute);
 app.use("/niko/v1",dashboardRoute);
 
 
-
-
-
 // ----------------- Serve React in Production -----------------
-if (process.env.NODE_ENV === "production") {
-  const reactBuildPath = path.join(__dirname, "my-app/build");
 
-  // Serve static files
-  app.use(express.static(reactBuildPath));
-
-  // Catch-all route for React (must be last)
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(reactBuildPath, "index.html"));
-  });
-}
+app.get("/", (req,res) => {
+  app.use(express.static(path.resolve(__dirname,"my-app","build")));
+  res.sendFile(path.resolve(__dirname,"my-app","build","index.html"));
+});
 
 // ----------------- Start Server -----------------
 const PORT = process.env.PORT || 5000;
